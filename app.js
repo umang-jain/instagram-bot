2a53b2dd28119295a3433610f0bddbb2e7b1292a
umang-jain <umang.usict.042164@ipu.ac.in>
2020-05-21 03:18:12 +0530

    add like feature

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const ig = require('./instagram');
+const config = require('./config');
+
 
 (async () => {
 
   await ig.initialize();
 
-  debugger;
-  
+  await ig.login(config.username,config.pass);
+
+  await ig.likeTagsProcess(['harrypotter','jkrowling']);
+
 })()
--- instagram.js
+++ instagram.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
 
 const BASE_URL = 'https://www.instagram.com/';
+const TAG_URL = (tag) => `https://www.instagram.com/explore/tags/${tag}/`;
+
 
 const instagram = {
   browser:null,
@@ -13,10 +15,67 @@ const instagram = {
 
     instagram.page = await instagram.browser.newPage();
 
+  },
+
+  login: async (username,password) => {
     await instagram.page.goto(BASE_URL,{
       waitUntill:'networkidle2'
     });
+
+    await instagram.page.waitFor(1000);
+
+    await instagram.page.type('input[name="username"]',username, {delay:100});
+    await instagram.page.type('input[name="password"]',password, {delay:100});
+    
+    await instagram.page.waitFor(500);
+
+    await instagram.page.evaluate( () => {
+      Array.from( document.querySelectorAll('button[type="submit"]'))[0].click();
+    });
+
+    await instagram.page.waitFor(10000);
+    // await instagram.page.waitFor('a > span[aria-label="Profile"]');
+    // let loginButton = await instagram.page.$x('//button[contains(text(),"Log In")]');
+    // await loginButton[0].click();
+  },
+  
+  likeTagsProcess: async (tags = ["harrypotter"]) => {
+    for(let tag of tags){
+
+      await instagram.page.goto(TAG_URL(tag), { waitUntill:'networkidle2' });
+      await instagram.page.waitFor(1000);
+
+      let posts = await instagram.page.$$('article > div:nth-child(3) img[decoding="auto"]');
+
+      for(let i=0;i<5;i++){
+        let post = posts[i];
+
+        await instagram.page.waitFor(2000);
+
+        await post.click();
+        // await instagram.page.waitFor('body[style="overflow:hidden;"]');
+        await instagram.page.waitFor(4000);
+
+        // let isLikable = await post.$('span[aria-label="Like"]');
+
+        // if(isLikable){
+        //   await post.click('span[aria-label="Like"]');
+        // }
+        await instagram.page.evaluate( () => {
+          Array.from( document.querySelectorAll('span button'))[0].click();
+        });
+
+        await instagram.page.waitFor(2000);
+
+        await instagram.page.evaluate( () => {
+          let temp = Array.from(document.querySelectorAll('button[type = "button"]'));
+          temp[temp.length - 1].click();
+        });
+        
+      }
+    }
   }
+
 }
 
 module.exports = instagram;
